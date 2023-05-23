af37c0a02054a0932cab377ab243e0988578dacf
Matheus Henrique <110773304+HenriqueCortazzo@users.noreply.github.com>
2023-05-23 18:29:18 -0300

    teste
    teste

---
+++ projetoUnifg/nbproject/src/projetounifg/ProjetoUnifg.java
@@ -0,0 +1,12 @@
+package projetounifg;
+
+import javax.swing.SwingUtilities;
+
+public class ProjetoUnifg {
+
+    public static void main(String[] args) {
+        paginaLogin frame = new paginaLogin();
+        frame.setVisible(true);
+    }
+
+}
