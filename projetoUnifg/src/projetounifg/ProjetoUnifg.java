6f564074b1c485ec4e43d4a6426fce3b72507cc1
Matheus Henrique <110773304+HenriqueCortazzo@users.noreply.github.com>
2023-05-23 11:48:39 -0300

    teste commit 1

--- projetoUnifg/src/projetounifg/ProjetoUnifg.java
+++ projetoUnifg/src/projetounifg/ProjetoUnifg.java
@@ -5,8 +5,8 @@ import javax.swing.SwingUtilities;
 public class ProjetoUnifg {
 
     public static void main(String[] args) {
-        paginaLogin frame = new paginaLogin();
-        frame.setVisible(true);
+//        paginaLogin frame = new paginaLogin();
+//        frame.setVisible(true);
     }
 
 }
