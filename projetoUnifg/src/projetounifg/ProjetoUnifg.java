55e308daddb972158ba0ec9d4f8755341e51da67
Matheus Henrique <110773304+HenriqueCortazzo@users.noreply.github.com>
2023-05-25 18:11:23 -0300

    alteracao

---
+++ projetoUnifg/src/projetounifg/ProjetoUnifg.java
@@ -0,0 +1,11 @@
+package projetounifg;
+
+import javax.swing.SwingUtilities;
+
+public class ProjetoUnifg {
+
+    public static void main(String[] args) {
+        new telaCadastro().setVisible(true);
+    }
+
+}
