34806e16af97fab0a0dc76573afa49090795e170
Matheus Henrique <110773304+HenriqueCortazzo@users.noreply.github.com>
2023-06-18 14:31:42 -0300

    Atualizações na Classe Main e BancodeDadosCreate.

--- projetoUnifg/src/projetounifg/ProjetoUnifg.java
+++ projetoUnifg/src/projetounifg/ProjetoUnifg.java
@@ -1,10 +1,14 @@
 package projetounifg;
 
+import userActions.BancoDeDadosCreate;
 import javax.swing.SwingUtilities;
 
 public class ProjetoUnifg {
 
     public static void main(String[] args) {
+        BancoDeDadosCreate bd = new BancoDeDadosCreate();
+        bd.criarBancoDados();
+        
         new telaCadastro().setVisible(true);
     }
 
--- projetoUnifg/src/userActions/BancoDeDadosCreate.java
+++ projetoUnifg/src/userActions/BancoDeDadosCreate.java
@@ -11,29 +11,9 @@ public class BancoDeDadosCreate {
 
     String sql = "CREATE DATABASE `sistemacadastro` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;";
 
-    String sql3 = "CREATE TABLE `sistemacadastro` (\n"
-            + "  `nome_aluno` varchar(200) DEFAULT NULL,\n"
-            + "  `ra_Aluno` int(11) NOT NULL,\n"
-            + "  `curso` varchar(45) NOT NULL,\n"
-            + "  `data_Nascimento` varchar(45) NOT NULL,\n"
-            + "  `tell` varchar(60) NOT NULL,\n"
-            + "  `municipio` varchar(45) NOT NULL,\n"
-            + "  `cpf` varchar(200) NOT NULL,\n"
-            + "  `periodo` varchar(45) NOT NULL,\n"
-            + "  `campus` varchar(45) NOT NULL,\n"
-            + "  `uf` varchar(45) NOT NULL,\n"
-            + "  `nota` varchar(45) DEFAULT NULL,\n"
-            + "  `faltas` int(11) NOT NULL,\n"
-            + "  `status` varchar(45) DEFAULT NULL,\n"
-            + "  PRIMARY KEY (`ra_Aluno`)\n"
-            + ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;";
+    String sql3 = "CREATE TABLE `sistemacadastro` ( `nome_aluno` varchar(200) DEFAULT NULL, `ra_Aluno` int(11) NOT NULL,`curso` varchar(45) NOT NULL,`data_Nascimento` varchar(45) NOT NULL,`tell` varchar(60) NOT NULL,`municipio` varchar(45) NOT NULL,`cpf` varchar(200) NOT NULL,`periodo` varchar(45) NOT NULL,`campus` varchar(45) NOT NULL,`uf` varchar(45) NOT NULL, `nota` varchar(45) DEFAULT NULL,`faltas` int(11) NOT NULL,`status` varchar(45) DEFAULT NULL, PRIMARY KEY (`ra_Aluno`)) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;";
 
-    String sql4 = "CREATE TABLE `usuario` (\n"
-            + "  `usuario_id` int(11) NOT NULL AUTO_INCREMENT,\n"
-            + "  `usuario` varchar(200) NOT NULL,\n"
-            + "  `senha` varchar(32) NOT NULL,\n"
-            + "  PRIMARY KEY (`usuario_id`)\n"
-            + ") ENGINE=InnoDB AUTO_INCREMENT=45 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;";
+    String sql4 = "CREATE TABLE `usuario` (`usuario_id` int(11) NOT NULL AUTO_INCREMENT,`usuario` varchar(200) NOT NULL,`senha` varchar(32) NOT NULL,PRIMARY KEY (`usuario_id`)) ENGINE=InnoDB AUTO_INCREMENT=45 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;";
 
     String sql2 = "USE `sistemacadastros`";
 
