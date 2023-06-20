bee3d5b7e13b36d8e32dc22d86413f2b5cbe7d99
Matheus Henrique <110773304+HenriqueCortazzo@users.noreply.github.com>
2023-06-20 19:11:29 -0300

    Criação da Classe usuario_Id Utilizada para realizar vínculo de conta.

---
+++ projetoUnifg/src/userActions/usuario_Id.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package userActions;
+
+/**
+ *
+ * @author MatheusWP
+ */
+public class usuario_Id {
+       private static int usuario_Id;
+
+    public static int getID() {
+        return usuario_Id;
+    }
+
+    public static void setID(int id) {
+        usuario_Id = id;
+    }
+}
